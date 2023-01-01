7ef0c85849b48fbe04377d025eaebc3b91e0d933
Junho Kim <junho4623@gmail.com>
2023-01-01 19:38:37 +0900

    installed flask_cors, pytest, bcrypt, jwt, sqlalchemy

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# index.py is equal to main.py in replit
-# print("Hello Python")
-
 from app import *
 # from app_book_ver import *
 
+from examples import endpoint_setup
 
 app = create_app()
 app.run(host='0.0.0.0', port=5000, debug=True)

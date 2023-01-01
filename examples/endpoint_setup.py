3c4ddb42382c32d9dee474aa980e1b4acdcc767c
Junho Kim <junho4623@gmail.com>
2023-01-01 19:52:15 +0900

    flask api test run successful

--- examples/endpoint_setup.py
+++ examples/endpoint_setup.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 app.users = {}
 app.id_count = 1
 
-@app.route("/ping", method=['GET'])
+@app.route("/ping", methods=['GET'])
 def ping():
     return "pong"
 
-@app.route("/sign-up", method=['POST'])
+@app.route("/sign-up", methods=['POST'])
 def sign_up():
     new_user = request.json
     new_user["id"] = app.id_count
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from app import *
+# from app import *
 # from app_book_ver import *
 
-from examples import endpoint_setup
+from examples.endpoint_setup import *
 
-app = create_app()
+# app = create_app()
 app.run(host='0.0.0.0', port=5000, debug=True)
 
